2a497704ae14fc65e2e7f1a8e518cdb13bf440a8
John Vincent Fernandez <cent2931@gmail.com>
2021-02-19 02:12:46 +0800

    created cartItem model

---
+++ app/src/main/java/com/bruntworktest/android/model/CartItem.kt
@@ -0,0 +1,10 @@
+package com.bruntworktest.android.model
+
+/**
+ * Created by John Vincent Fernandez on 02/16/2021.
+ */
+class CartItem(
+        var product: Product,
+        var qty: Int = 0
+) {
+}
