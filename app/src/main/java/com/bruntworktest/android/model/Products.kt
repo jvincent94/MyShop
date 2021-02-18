aa5a65ea816fbb69e7ac3fd2aafe796e7b135f85
John Vincent Fernandez <cent2931@gmail.com>
2021-02-19 02:13:17 +0800

    created products model

--- app/src/main/java/com/bruntworktest/android/model/Products.kt
+++ app/src/main/java/com/bruntworktest/android/model/Products.kt
@@ -1,9 +1,18 @@
 package com.bruntworktest.android.model
 
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created by John Vincent Fernandez on 02/12/2021.
+ */
 data class Products(
-    val id: String,
-    val name: String,
-    val category: String,
-    val price: String,
-    val bgColor: String
+    @SerializedName("products") val products: List<Product>
+)
+
+data class Product(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("category") val category: String,
+    @SerializedName("price") val price: String,
+    @SerializedName("bgColor") val bgColor: String
 )
