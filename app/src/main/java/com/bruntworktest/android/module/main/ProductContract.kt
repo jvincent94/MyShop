8fe5854f7068f9556c45218b90d3cc3bc1411c4a
John Vincent Fernandez <cent2931@gmail.com>
2021-02-19 02:17:46 +0800

    created product contract

---
+++ app/src/main/java/com/bruntworktest/android/module/main/ProductContract.kt
@@ -0,0 +1,25 @@
+package com.bruntworktest.android.module.main
+
+import com.bruntworktest.android.base.BaseMvpPresenter
+import com.bruntworktest.android.base.BaseMvpView
+import com.bruntworktest.android.model.CartItem
+import com.bruntworktest.android.model.Product
+import com.bruntworktest.android.model.Products
+
+/**
+ * Created by John Vincent Fernandez on 02/16/2021.
+ */
+object ProductContract {
+
+    interface View : BaseMvpView {
+        fun showProducts(products: MutableList<Product>)
+        fun showCartCount(count: Int)
+    }
+
+    interface Presenter: BaseMvpPresenter<View>{
+        fun init()
+        fun loadProducts()
+        fun addProduct(cartItem: CartItem)
+        fun deleteProduct(cartItem: CartItem)
+    }
+}
