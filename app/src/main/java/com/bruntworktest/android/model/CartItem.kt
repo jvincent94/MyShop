b6dd5874f229d81050170d10f151fa7bd10a3c9f
John Vincent Fernandez <cent2931@gmail.com>
2021-02-19 19:15:19 +0800

    reverted to Product Object

--- app/src/main/java/com/bruntworktest/android/model/CartItem.kt
+++ app/src/main/java/com/bruntworktest/android/model/CartItem.kt
@@ -3,8 +3,8 @@ package com.bruntworktest.android.model
 /**
  * Created by John Vincent Fernandez on 02/16/2021.
  */
-class CartItem(
-        var product: Product,
-        var qty: Int = 0
+data class CartItem(
+    var product: Product,
+    var qty: Int = 0
 ) {
 }
